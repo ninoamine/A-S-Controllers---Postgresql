9ac6c2aa68dceb6c2e1ccf4cfaa26c2db7f3ac67
Amine Bakkali <abakkali@ABAKKALI1.emea.cegedim.grp>
2025-08-04 09:51:59 +0200

    update controller

--- pkg/controller/postgresql_controller.go
+++ pkg/controller/postgresql_controller.go
@@ -2,6 +2,8 @@ package controller
 
 import (
     "time"
+	"context"
+	"fmt"
     
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "k8s.io/apimachinery/pkg/runtime"
@@ -47,5 +49,90 @@ func NewPostgreSQLController(kubeClient kubernetes.Interface, restClient rest.In
         30*time.Second, // Resync period
         cache.Indexers{},
     )
+	controller.informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+        AddFunc:    controller.onAdd,
+        UpdateFunc: controller.onUpdate,
+        DeleteFunc: controller.onDelete,
+    })
 	return controller
+}
+// listPostgreSQLDatabases liste les ressources selon la configuration namespace
+func (c *PostgreSQLController) listPostgreSQLDatabases(options metav1.ListOptions) (runtime.Object, error) {
+    if len(c.watchNamespaces) == 0 {
+        // Tous les namespaces - utilise cluster-scoped list
+        result := &api.PostgreSQLDatabaseList{}
+        err := c.restClient.Get().
+            Resource("postgresqldatabases").
+            VersionedParams(&options, metav1.ParameterCodec).
+            Do(context.TODO()).
+            Into(result)
+        return result, err
+    }
+    
+    // Namespaces spécifiques - combine les résultats
+    allDatabases := &api.PostgreSQLDatabaseList{}
+    for _, ns := range c.watchNamespaces {
+        result := &api.PostgreSQLDatabaseList{}
+        err := c.restClient.Get().
+            Namespace(ns).
+            Resource("postgresqldatabases").
+            VersionedParams(&options, metav1.ParameterCodec).
+            Do(context.TODO()).
+            Into(result)
+        if err != nil {
+            return nil, err
+        }
+        allDatabases.Items = append(allDatabases.Items, result.Items...)
+    }
+    return allDatabases, nil
+}
+
+// watchPostgreSQLDatabases watch les changements sur les ressources
+func (c *PostgreSQLController) watchPostgreSQLDatabases(options metav1.ListOptions) (watch.Interface, error) {
+    if len(c.watchNamespaces) == 0 {
+        // Tous les namespaces
+        return c.restClient.Get().
+            Resource("postgresqldatabases").
+            VersionedParams(&options, metav1.ParameterCodec).
+            Watch(context.TODO())
+    }
+    
+    // Pour plusieurs namespaces, on va watcher le premier pour l'instant
+    // (Une vraie implémentation multi-namespace est plus complexe)
+    return c.restClient.Get().
+        Namespace(c.watchNamespaces[0]).
+        Resource("postgresqldatabases").
+        VersionedParams(&options, metav1.ParameterCodec).
+        Watch(context.TODO())
+}
+
+
+// onAdd est appelé quand une nouvelle ressource PostgreSQLDatabase est créée
+func (c *PostgreSQLController) onAdd(obj interface{}) {
+    key, err := cache.MetaNamespaceKeyFunc(obj)
+    if err != nil {
+        fmt.Printf("Erreur lors de la création de la clé: %v\n", err)
+        return
+    }
+    c.queue.Add(key)
+}
+
+// onUpdate est appelé quand une ressource PostgreSQLDatabase est modifiée
+func (c *PostgreSQLController) onUpdate(oldObj, newObj interface{}) {
+    key, err := cache.MetaNamespaceKeyFunc(newObj)
+    if err != nil {
+        fmt.Printf("Erreur lors de la création de la clé: %v\n", err)
+        return
+    }
+    c.queue.Add(key)
+}
+
+// onDelete est appelé quand une ressource PostgreSQLDatabase est supprimée
+func (c *PostgreSQLController) onDelete(obj interface{}) {
+    key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+    if err != nil {
+        fmt.Printf("Erreur lors de la création de la clé: %v\n", err)
+        return
+    }
+    c.queue.Add(key)
 }
